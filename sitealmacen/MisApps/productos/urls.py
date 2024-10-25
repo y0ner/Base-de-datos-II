7053c83342b9b50224fd84a6e6bdebd7fc643a16
yoner <yonerisaza9@gmail.com>
2024-10-25 11:07:01 -0500

    Removed tracking of the ./clase directory

--- sitealmacen/MisApps/productos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-#from MisApps.productos.views import ProductoView
-
-
-
-urlpatterns = [
-   # path('', ProductoView.as_view()),
-]
--- sitealmacen/MisApps/ventas/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from MisApps.ventas.views import home
-
-urlpatterns = [
-    path('inicio/', home, name= 'home'),
-]
-
