9dd61d5d5e01da5a396e251533a916cdf40b0d6f
yoner <yonerisaza9@gmail.com>
2024-10-24 22:06:00 -0500

    ultimo cambio 10:05pm

--- gestion_inventario/MisApps/facturas/serializers.py
+++ gestion_inventario/MisApps/facturas/serializers.py
@@ -1,11 +1,17 @@
-from dataclasses import field
-from statistics import mode
-
 from rest_framework import serializers
 from MisApps.facturas.models import Factura
+from MisApps.clientes.models import Cliente
+from MisApps.clientes.serializers import ClienteSerializer
 
 class FacturaSerializer(serializers.ModelSerializer):
-    # len_nombreCliente = serializers.SerializerMethodField()
+    cliente = serializers.PrimaryKeyRelatedField(queryset=Cliente.objects.all())  # Solo acepta el ID de cliente en solicitudes
+
     class Meta:
         model = Factura
-        fields = "__all__"  #incluir todos los campos
+        fields = "__all__"
+
+    # Cambia la representación para devolver el objeto cliente completo en la respuesta
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['cliente'] = ClienteSerializer(instance.cliente).data
+        return representation
--- gestion_inventario/MisApps/productos/serializers.py
+++ gestion_inventario/MisApps/productos/serializers.py
@@ -3,10 +3,18 @@ from statistics import mode
 
 from rest_framework import serializers
 from MisApps.productos.models import Producto
+from MisApps.categorias.models import Categoria
 from MisApps.categorias.serializers import CategoriaSerializer
 
 class ProductoSerializer(serializers.ModelSerializer):
-    #categoria = CategoriaSerializer()  # Anidar el serializador de categoria
+    categoria = serializers.PrimaryKeyRelatedField(queryset=Categoria.objects.all())  # Acepta solo el ID de categoría en la solicitud
+
     class Meta:
         model = Producto
-        fields = "__all__"  #incluir todos los campos
+        fields = "__all__"
+
+    # Esto convierte el ID en un objeto completo al devolver la respuesta
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['categoria'] = CategoriaSerializer(instance.categoria).data
+        return representation
